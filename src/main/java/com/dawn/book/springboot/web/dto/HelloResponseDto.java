742450ed5ea3f6a2721191510eda6ea336359d4a
dawn <myorange06@gmail.com>
2020-08-16 15:20:04 +0900

    ch5 oauth프로퍼티 git ignore등록

---
+++ src/main/java/com/dawn/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.dawn.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
